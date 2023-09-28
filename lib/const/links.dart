1dd3dc6b319da29fc66ff242db9aa674aa243220
Delarram <tunyanaing960@gmail.com>
2023-09-28 15:50:21 +0630

    added navbar and links

---
+++ lib/const/links.dart
@@ -0,0 +1,25 @@
+
+const String resume =
+    '';
+
+const String gitHub = 'h';
+
+const String facebook = '';
+
+const String twitter = '';
+
+const String youtube =
+    '';
+
+const String linkedin = '';
+
+
+const String instagram = '';
+
+const String profileImage =
+    "";
+
+const String fiverr = '';
+
+
+const String whatsapp = 'https://api.whatsapp.com/send?phone=09793346005';
