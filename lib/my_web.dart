0175189aa5ffc6e32cc65891962ddfdcb912f822
Delarram <tunyanaing960@gmail.com>
2023-09-28 14:57:05 +0630

    added state and cubit

---
+++ lib/my_web.dart
@@ -0,0 +1,34 @@
+import 'package:black_poudel_portofolio/theme/cubit/theme_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sizer/sizer.dart';
+
+
+class MyWeb extends StatelessWidget {
+  const MyWeb({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+
+      ],
+      child: MultiProvider(
+        providers: [
+
+        ],
+        child: BlocBuilder<ThemeCubit, ThemeState>(builder: (context, state) {
+          return Sizer(builder: (context, orientation, deviceType) {
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Delarram',
+              initialRoute: "/",
+
+            );
+          });
+        }),
+      ),
+    );
+  }
+}
